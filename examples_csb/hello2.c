b04d2f25c2dda07b2331a323bcf2d8b6c2824b57
Martin Billinger <flkazemakase@gmail.com>
2017-08-04 13:18:07 +0200

    Cleaned up hello2 example

--- examples_csb/hello2.c
+++ examples_csb/hello2.c
@@ -2,21 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <unistd.h>
-
-void memdump();
-void memdiff(size_t offset, size_t n);
-
-int test() {
-    int x = 42;
-    //printf("ABCDEFGHIJKLMNOPQRSTOVWXYZ\n", x);
-    return x;
-}
-
 int main () {
-    char *name = malloc(20);
-    memset(name, 255, 20);
-    strncpy(name, "malloc people", 15);
+    char *name;
+    name = malloc(20);
+    strncpy(name, "malloc people", 14);
     printf("hello %s\n", name);
     return 2;
 }
